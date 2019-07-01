eb92c7080e9d0a9585d00f4f0c7d61e5b9b22871
Pano <pano_skylakis@outlook.com>
2019-07-01 18:19:05 +1200

    bug fix

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import {render} from 'react-dom'
+import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(
+  ReactDOM.render(
     <App />,
     document.getElementById('app')
   )
--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const path = require('path')
-
-const cors = require('cors')
+// const cors = require('cors')
 const express = require('express')
 
 const server = express()
 
-server.use(cors({origin: 'http://localhost:8080'}))
+// server.use(cors({origin: 'http://localhost:8080'}))
 server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
