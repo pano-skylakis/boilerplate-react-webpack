c94dc5cd96191cbccd12f15fcca07f729c172058
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2019-08-28 19:26:05 +1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const path = require('path')
-// const cors = require('cors')
 const express = require('express')
 
 const server = express()
 
-// server.use(cors({origin: 'http://localhost:8080'}))
+
 server.use(express.static('public'))
 
 
